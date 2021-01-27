bf16fbfb63bec944cf3adc9049995ed65a47765e
Katia K <katya1242@yahoo.com>
2021-01-26 17:26:20 -0800

    Removed key from settings.py

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x*m0q*r4rtl2*ilf5n=m+i@1xjqnp^q138x)k0suec+9vyd9yj'
+SECRET_KEY = "pass"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
